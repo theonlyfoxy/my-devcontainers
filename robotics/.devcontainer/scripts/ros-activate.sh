8f8ec4cf0113a1e369ce1f35ea1c5bf36b9e1f6c
Foxy <firebot1020@gmail.com>
2022-06-27 22:59:11 +0430

    Add VCPKG Support

--- robotics/.devcontainer/scripts/ros-activate.sh
+++ robotics/.devcontainer/scripts/ros-activate.sh
@@ -2,5 +2,5 @@ if [ -f /opt/ros/$ROS_DISTRO/setup.bash ]; then
   . /opt/ros/$ROS_DISTRO/setup.bash
 fi
 
-export PATH=/usr/lib/ccache:${PATH}
+export PATH=/usr/lib/ccache:/vcpkg:${PATH}
 export RCUTILS_COLORIZED_OUTPUT=1
